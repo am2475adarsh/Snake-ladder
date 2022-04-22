2154fadb433b86074c2aac5f97d189583385ba99
Adarsh Mohanty <am2475@srmist.edu.in>
2022-04-22 23:35:24 +0530

    Main logic

--- script.js
+++ script.js
@@ -1,74 +1,62 @@
-// var player_1, player_2, player_3, player_4;
+'use strict';
 let players = ["player_1", "player_2", "player_3", "player_4"];
 let cell_count = [-1, -1, -1, -1];
 let six_case = 0;
+console.log(cell_count);
+let i = 0;
 
 // start the game fr
 document.querySelector('.start').addEventListener('click', function () {
+    document.querySelector('.start').style.backgroundColor = '#60b347';
 
-    var i = 0;
+    // dice roll
+    document.querySelector('.dice').addEventListener('click', function () {
+        document.querySelector('.dice').style.backgroundColor = '#c7600c';
 
-    // check if any player completed tabhi dice roll karo 
-    while (cell_count[0] != 100 || cell_count[1] != 100 || cell_count[2] != 100 || cell_count[3] != 100) {
+        // number generator
+        const number = Math.trunc(Math.random() * 6) + 1;
+        console.log(number, i);
 
-        // to alternate the players
-        for (i = 0; i < 3; i++) {
+        if (number === 6) {
+            // if player still  in lobby
+            if (cell_count[i] === -1) {
 
-            // dice roll
-            document.querySelector('.dice').addEventListener('click', function () {
+                // move player to home
+                cell_count[i] = 0;
+                six_case = cell_count[i];
 
-                // number generator
-                const number = Math.trunc(Math.random() * 6) + 1;
-
-                function number_six(six_case) {
-                    document.querySelector('.dice').addEventListener('click', function () {
-
-                        const number = Math.trunc(Math.random() * 6) + 1;
-                        if (number === 6) {
-                            six_case = six_case + number;
-                            number_six(six_case);
-                        }
-                        else {
-                            six_case = six_case + number;
-                        }
-
-                    })
-                    return six_case;
+            }
+            else {
+                // if player not in lobby
+                cell_count[i] = cell_count[i] + 6;
+                if (cell_count[i] == 100) {
+                    alert("Game Over hehe!");
                 }
-
-                if (number === 6) {
-                    // if player still  in lobby
-                    if (cell_count[i] === -1) {
-
-                        // move player to lobby
-                        cell_count[i] = 0;
-                        six_case = cell_count[i];
-
-                        // function six called 
-                        number_six(six_case);
-                    }
-                    else {
-                        // if player not in lobby
-                        cell_count[i] = cell_count + 6;
-                        number_six(six_case);
-                    }
-
-                }
-
-                if (number != 6) {
-
-                    // if player in lobby and not six
-                    if (cell_count[i] === -1) {
-                        cell_count[i] === -1;
-                    }
-                    else {
-
-                        // if player not in lobby and not six
-                        cell_count[i] = cell_count[i] + number;
-                    }
-                }
-            })
+            }
+            // We'll only need to increment i value if the player didn't get 6 cause if its a 6 he needs to repeat the value till the number is not = 6 
         }
-    }
+
+        // If we didn't get 6 in dice roll.
+        if (number != 6) {
+
+            // if player in lobby and not six
+            if (cell_count[i] === -1) {
+                cell_count[i] = -1;
+            }
+            else {
+
+                // if player not in lobby and not six
+                cell_count[i] = cell_count[i] + number;
+                if (cell_count[i] == 100) {
+                    alert("Game Over hehe!");
+                }
+            }
+
+            i = (++i) % 4;
+
+        }
+
+        console.log(cell_count);
+    })
 
 })
